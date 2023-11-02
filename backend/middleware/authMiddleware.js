68444654be817c5457f9f3c957d6855bc87f44da
Jalal Alkadi <55977103+KinguJ@users.noreply.github.com>
2023-11-02 12:57:35 +0300

    a

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ const protect = asyncHandler(async (req, res, next) => {
   token = req.cookies.jwt;
 
   if (token) {
-    console.log(token);
+    console.log(process.env.JWT_SECRET);
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
